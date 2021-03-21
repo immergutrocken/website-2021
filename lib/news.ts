6267477c46135dae03c441fde398b3fc742ae2c1
Hagen Strahl <hagen.strahl@gmail.com>
2021-03-21 17:02:21 +0100

    set env variables correctly

--- lib/news.ts
+++ lib/news.ts
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 const client = sanityClient({
-  projectId: process.env.SANITY_DATASET,
-  dataset: "production",
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   useCdn: true,
 });
 
