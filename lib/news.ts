c0f485b1b3fa3b6754d831861ac6be5d340d9fa5
Hagen Strahl <hagen.strahl@gmail.com>
2021-03-28 15:26:05 +0200

    update sanity query for news

--- lib/news.ts
+++ lib/news.ts
@@ -7,6 +7,13 @@ export interface NewsLink {
 
 export const getNewsList = async (): Promise<NewsLink[]> => {
   const query =
-    "*[_type == 'article' && isNews == true] {'title': languages.de.title, 'slug': slug.current}";
-  return client.fetch(query);
+    "*[_type == 'sortings'] {'title': news[]->languages.de.title, 'slug': news[]->slug.current}";
+  const result = await client.fetch(query);
+  const newArray = result[0].slug.map(
+    (slug: string, index: number): NewsLink => ({
+      title: result[0].title[index],
+      slug: slug,
+    })
+  );
+  return newArray;
 };
