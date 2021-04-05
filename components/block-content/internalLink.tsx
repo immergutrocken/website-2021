d9aae6d5c6ad25f9dfa76d7c89c07e51eee1ebd4
Hagen Strahl <hagen.strahl@gmail.com>
2021-04-05 20:02:33 +0200

    add link component for block content

--- components/block-content/internalLink.tsx
+++ components/block-content/internalLink.tsx
@@ -1,3 +1,19 @@
-const InternalLink = (): JSX.Element => <span>Internal Link coming soon!</span>;
+import NextLink from "next/link";
+
+interface InternalLinkProps {
+  mark: {
+    docType: string;
+    slug: string;
+  };
+  children: string;
+}
+
+const InternalLink = ({ mark, children }: InternalLinkProps): JSX.Element => {
+  return (
+    <NextLink href={`/${mark.docType}/${mark.slug}`}>
+      <a className="font-bold">{children}</a>
+    </NextLink>
+  );
+};
 
 export default InternalLink;
--- components/shared/link.tsx
+++ components/shared/link.tsx
@@ -15,7 +15,7 @@ const Link = ({
     href={href}
     target="_blank"
     rel="noreferrer"
-    className={`hover:underline focus:outline-none ${className}`}
+    className={`hover:underline focus:outline-none ${className} font-bold`}
     title={title}
   >
     {children}
