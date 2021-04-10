dc64c3c109e79fd07ab9b5e3b95b05818434ec15
Hagen Strahl <hagen.strahl@gmail.com>
2021-04-10 14:02:25 +0200

    remove obsolete redirect

--- next.config.js
+++ next.config.js
@@ -2,12 +2,4 @@ module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/artist/josé-gonzález",
-        destination: "/artist/jose-gonzalez",
-      },
-    ];
-  },
 };
