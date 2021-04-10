b324396352437532b2d3bd5ffe37e5c601b8bad7
Hagen Strahl <hagen.strahl@gmail.com>
2021-04-10 13:26:07 +0200

    redirect jose gonzales

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/artist/josé-gonzález",
+        destination: "/artist/jose-gonzalez",
+        permanent: true,
+      },
+    ];
+  },
 };
